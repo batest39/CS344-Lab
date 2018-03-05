5bf2c88ded956ad1476f19ce8c3d3c9b4c3e9f29
batest39 <batest39@uwosh.edu>
2018-03-05 16:00:44 -0600

    Cards outside of hand are hidden and the ones in the hand are randomly generated properly. TODO: make sure the cards aren't duplicates

--- app/src/main/java/com/example/batest39/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/batest39/myfirstapplication/MainActivity.java
@@ -4,10 +4,19 @@ import android.content.res.Configuration;
 import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView card1;
+    private ImageView card2;
+    private ImageView card3;
+    private ImageView display1;
+    private ImageView display2;
+    private ImageView warCard1;
+    private ImageView warCard2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,24 +24,44 @@ public class MainActivity extends AppCompatActivity {
         if(getResources().getConfiguration().orientation==
                 Configuration.ORIENTATION_LANDSCAPE) {
             setContentView(R.layout.activity_landscape);
-            getRandomCards();
+            setCards();
+            getRandomCards(card1, card2, card3);
         } else{
             setContentView(R.layout.activity_portrait);
-            getRandomCards();
+            setCards();
+            getRandomCards(card1, card2, card3);
         }
         //this leaves the keyboard hidden on load
         getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 
-    private void getRandomCards(){
+    private void setCards(){
+        this.card1 = (ImageView) findViewById(R.id.card1);
+        this.card2 = (ImageView) findViewById(R.id.card2);
+        this.card3 = (ImageView) findViewById(R.id.card3);
+
+        this.display1 = (ImageView) findViewById(R.id.display1);
+        this.display1.setImageDrawable(null);
+
+        this.display2 = (ImageView) findViewById(R.id.display2);
+        this.display2.setImageDrawable(null);
+
+        this.warCard1 = (ImageView) findViewById(R.id.warCard1);
+        this.warCard1.setImageDrawable(null);
+
+        this.warCard2 = (ImageView) findViewById(R.id.warCard2);
+        this.warCard2.setImageDrawable(null);
+    }
+
+    private void getRandomCards(ImageView card1, ImageView card2, ImageView card3){
         TypedArray cardImageArray = getResources().obtainTypedArray(R.array.cards);
         //TypedArray cardsInHand = getResources().obtainTypedArray(R.array.cardsInHand);
         ImageView[] cardsInHand = new ImageView[3];
 
-        cardsInHand[0] = (ImageView) findViewById(R.id.card1);
-        cardsInHand[1] = (ImageView) findViewById(R.id.card2);
-        cardsInHand[2] = (ImageView) findViewById(R.id.card3);
+        cardsInHand[0] = card1;
+        cardsInHand[1] = card2;
+        cardsInHand[2] = card3;
 
 
         for (int i = 0; i < 3; i++){
@@ -41,4 +70,10 @@ public class MainActivity extends AppCompatActivity {
             cardsInHand[i].setImageResource(cardImageArray.getResourceId(rand, R.drawable.c1));
         }
     }
+
+/*    public void onTouch(View view){
+        if (view == findViewById(this.card1.getId())){
+
+        }
+    }*/
 }
